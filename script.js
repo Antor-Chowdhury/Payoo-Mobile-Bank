3c47c7b7ced00c011a885622b4a481d44a20b2aa
Antor-Chowdhury <antor1347@gmail.com>
2025-08-23 22:41:09 +0600

    add dom functionality in Login

--- script.js
+++ script.js
@@ -4,6 +4,27 @@ document
   .getElementById("btn-login")
   .addEventListener("click", function (event) {
     event.preventDefault();
-    console.log("login button clicked");
-    console.log(event);
+
+    const mobileNumber = 01776218402;
+    const pinNumber = 1234;
+
+    const mobileNumberValue = document.getElementById("mobile-number"); // this value will be in string
+
+    const mobileNumberValueConverted = parseInt(mobileNumberValue.value); // converted the value to int for comparing with mobileNumber value.
+
+    const pinNumberValue = document.getElementById("pin-number");
+    const pinNumberValueConverted = parseInt(pinNumberValue.value);
+
+    if (
+      mobileNumberValueConverted === mobileNumber &&
+      pinNumberValueConverted === pinNumber
+    ) {
+      window.location.href = "./home.html";
+    } else {
+      alert("Invalid credential");
+    }
+
+    // clears the input value each time
+    mobileNumberValue.value = "";
+    pinNumberValue.value = "";
   });
